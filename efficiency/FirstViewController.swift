973ae4b96e03824108e4e0f5d7c3d25f42145536
Glynn Jordan <glynn.jordan@accenture.com>
2019-08-11 14:35:40 -0500

    Initial Commit

---
+++ efficiency/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  efficiency
+//
+//  Created by Glynn Jordan on 8/11/19.
+//  Copyright © 2019 Glynn Jordan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ efficiency/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  efficiency
+//
+//  Created by Glynn Jordan on 8/11/19.
+//  Copyright © 2019 Glynn Jordan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
